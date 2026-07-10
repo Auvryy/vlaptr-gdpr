10406b349e036ea8ef32e7a54eac15f9e611ca03
Auvryy <sarneandy6@gmail.com>
2026-07-10 20:05:49 +0800

    nav scraping

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,28 @@
-import { Menu } from "lucide-react";
+import { Menu, Search } from "lucide-react";
+import InputField from "./InputField";
+import { useState } from 'react'
 
 export default function NavBar() {
+  const [search, setSearch] = useState('');
+
   return (
-    <nav className="flex items-center justify-between p-3 bg-green-500">
-      <section className="flex items-center justify-start px-3 gap-5 w-[80%]">
+    <nav className="flex items-center justify-between px-10 py-5  bg-emerald-500 ">
+      <section className="flex items-center justify-start px-3  mr-15 gap-5 w-fit">
         <Menu className="text-white cursor-pointer" />
-        <p className="text-white text-lg font-semibold">Vlaptr</p>
+        <p className="text-white text-lg font-bold cursor-pointer">Vlaptr</p>
       </section>
-      <section className="w-full">
-        <input className="" />
+      <section className="relative w-full flex items-center rounded-sm ">
+        <InputField
+          label=""
+          type="text"
+          value={search}
+          onChange={setSearch}
+        />
+        <div className="absolute pl-3 pt-2 pointer-events-none">
+          <Search className="h-6 w-6 text-white" />
+        </div>
       </section>
-      <section className="w-full"></section>
+      <section className="w-fit"></section>
     </nav>
   );
 }
