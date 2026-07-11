fb00b2dd42075385233c275ec70e1a87d17242a1
Auvryy <sarneandy6@gmail.com>
2026-07-11 21:02:01 +0800

    added image carousel ad

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import LoginPage from "./pages/LoginPage"
 import HomePage from "./pages/HomePage"
 import { useAuthStore } from "./store/useAuthStore"
+import CartPage from "./pages/CartPage"
 
 function App() {
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn)
-  
 
-  return (
-    isLoggedIn ? <HomePage/> : <LoginPage/>
-  )
+  const [view, setView] = useState('home')
+
+  if (!isLoggedIn) return <LoginPage />
+
+  if (view === 'cart') return <CartPage />
+  return <HomePage onViewChange={setView} />
+
 }
 
 export default App
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,21 @@
+import BannerCarousel from "../components/BannerCarousel";
 import NavBar from "../components/NavBar";
 import SideBar from "../components/SideBar";
 
-export default function HomePage() {
+export default function HomePage({ onViewChange }: { onViewChange: (view: string) => void }) {
   return (
-    <div>
+    <div className="bg-emerald-50">
       {" "}
       {/* main body */}
-      <NavBar />
+      <NavBar onViewChange={onViewChange} />
       <div>
         {" "}
         {/* horizontal part side by side. */}
         <SideBar />
-        <main></main>
+        <main className="flex items-center justify-center px-20">
+          <BannerCarousel />
+
+        </main>
       </div>
     </div>
   );
